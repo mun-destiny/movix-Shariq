35fc5c4e16394cc7096a2534157b03d0e90f2b50
hidding-dev <huuphuoc274@gmail.com>
2023-08-11 10:42:51 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/movix-Shariq/'
+  }
+
+  return config
+})
+
+// https://github.com/ErickKS/vite-deploy
+// https://github.com/sitek94/vite-deploy-demo
+// https://blog.devgenius.io/how-to-deploy-your-vite-react-app-to-github-pages-with-and-without-react-router-b060d912b10e
